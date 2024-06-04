b20a74a3da0e01e238581c424f5704632edde1ac
cy <chanwit.yimneam@hotmail.com>
2024-06-04 14:05:47 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,20 @@
 export default function Home() {
   return (
     <div
+    m="4"
     p="4"
+    bg="gray-100"
     >
-      <p text="2xl">Hi</p>
+      <div><p text="2xl" >Hi 1</p></div>
+      <div><p text="2xl">Hi 2</p></div>
+      <div><p text="2xl">Test</p></div>
+      <span
+                className="group-hover:-translate-x-1 i-lucide:arrow-up-right"
+                inline="block"
+                transition="transform"
+                motion-reduce="transform-none"
+                m="l-1"
+              />
     </div>
   );
 }
--- uno.congig.ts
+++ uno.congig.ts
@@ -1,21 +1,22 @@
 import {
-	defineConfig,
-	presetAttributify,
-	presetIcons,
-	presetUno,
-      } from 'unocss'
-      
-      export default defineConfig({
-	presets: [
-	  presetUno({
-	    dark: 'media',
-	  }),
-	  presetAttributify(),
-	  presetIcons({
-	    extraProperties: {
-	      'display': 'inline-block',
-	      'vertical-align': 'middle',
-	    },
-	  }),
-	],
-      })
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+} from "unocss";
+
+export default defineConfig({
+  //   rules: [[/^m-([\.\d]+)$/, ([_, num]) => ({ margin: `${num}px` })]],
+  presets: [
+    presetUno({
+      dark: "media",
+    }),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: {
+        display: "inline-block",
+        "vertical-align": "middle",
+      },
+    }),
+  ],
+});
